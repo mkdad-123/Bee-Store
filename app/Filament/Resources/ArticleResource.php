1650eb675848d47e24eee52d3d86eb8a060fb022
mkdad-123 <ma0109424@gmail.com>
2025-03-24 09:42:08 -0800

    Delete config/filament-tiptap-editor.php and change navigation icon to article

--- app/Filament/Resources/ArticleResource.php
+++ app/Filament/Resources/ArticleResource.php
@@ -18,7 +18,7 @@ class ArticleResource extends Resource
 {
     protected static ?string $model = Article::class;
 
-    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+    protected static ?string $navigationIcon = 'heroicon-o-pencil-square';
 
     public static function form(Form $form): Form
     {
